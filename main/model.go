12702bc846e930be78a5b96175c0b0ecba920c8e
Anton Kachurin <anton.kachurin@t-systems.com>
2019-09-10 18:48:13 +0300

    Fix db integration

--- main/model.go
+++ main/model.go
@@ -43,23 +43,23 @@ func CreateTable(db *sql.DB) {
 }
 
 func (e *entity) getEntity(db *sql.DB) error {
-	return db.QueryRow("SELECT data FROM entity WHERE id like ($1)", e.Uuid).Scan(&e.Data)
+	return db.QueryRow("SELECT data FROM entity WHERE uuid like ($1)", e.Uuid).Scan(&e.Data)
 }
 
 func (e *entity) updateEntity(db *sql.DB) error {
-	_, err := db.Exec("UPDATE entity SET data=$1 WHERE id=$2", e.Data, e.Uuid)
+	_, err := db.Exec("UPDATE entity SET data=$1 WHERE uuid=$2", e.Data, e.Uuid)
 	return err
 }
 
 func (e *entity) deleteEntity(db *sql.DB) error {
-	_, err := db.Exec("DELETE FROM entity WHERE id=$1", e.Uuid)
+	_, err := db.Exec("DELETE FROM entity WHERE uuid=$1", e.Uuid)
 	return err
 }
 
 func (e *entity) createEntity(db *sql.DB) error {
 	// postgres doesn't return the last inserted Uuid so this is the workaround
 	_, err := db.Exec(
-		"INSERT INTO entity(id, data) VALUES($1, $2)", e.Uuid, e.Data)
+		"INSERT INTO entity(uuid, data) VALUES($1, $2)", e.Uuid, e.Data)
 	return err
 }
 
@@ -73,7 +73,7 @@ func isConnectionError(err error) bool {
 }
 
 func getEntities(db *sql.DB, start, count int) ([]entity, error) {
-	rows, err := db.Query("SELECT id, data FROM entity LIMIT $1 OFFSET $2", count, start)
+	rows, err := db.Query("SELECT uuid, data FROM entity LIMIT $1 OFFSET $2", count, start)
 	if err != nil {
 		if isConnectionError(err) {
 			return nil, errors.New("can't connect to database")
